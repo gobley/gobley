f192219f01842eb07dbdbd086b5650c2eb8ec6e7
Chanjung Kim <chanjungkim@paxbun.dev>
2025-03-05 19:08:29 +0900

    Fix the wrong project name in Gradle plugin tests

--- .github/workflows/pr-build-test-gradle-plugin-tests.ps1
+++ .github/workflows/pr-build-test-gradle-plugin-tests.ps1
@@ -1,2 +1,10 @@
-./gradlew :build-logic:gradle-plugin:test;
+$projectNames = @(
+    "gobley-gradle",
+    "gobley-gradle-cargo",
+    "gobley-gradle-rust",
+    "gobley-gradle-uniffi"
+);
+foreach ($projectName in $projectNames) {
+    & "./gradlew" ":build-logic:${projectName}:test";
+}
 ./.github/workflows/pr-build-test-copy-test-result.ps1;
