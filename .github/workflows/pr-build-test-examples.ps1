e8846511b12bfdcb01634825271f62acdb8cac1f
Chanjung Kim <chanjungkim@paxbun.dev>
2025-03-05 19:09:10 +0900

    Fix pwsh -c always returning 0 despite of test failures

--- .github/workflows/pr-build-test-examples.ps1
+++ .github/workflows/pr-build-test-examples.ps1
@@ -1,7 +1,13 @@
-./gradlew allTests `
-    "-Pgobley.projects.gradleTests=false" `
-    "-Pgobley.projects.uniffiTests=false";
-./.github/workflows/pr-build-test-copy-test-result.ps1;
-./gradlew clean `
-    "-Pgobley.projects.gradleTests=false" `
-    "-Pgobley.projects.uniffiTests=false";
+$ErrorActionPreference = "Stop";
+$PSNativeCommandUseErrorActionPreference = $true;
+
+try {
+    ./gradlew allTests `
+        "-Pgobley.projects.gradleTests=false" `
+        "-Pgobley.projects.uniffiTests=false";
+} finally {
+    ./.github/workflows/pr-build-test-copy-test-result.ps1;
+    ./gradlew clean `
+        "-Pgobley.projects.gradleTests=false" `
+        "-Pgobley.projects.uniffiTests=false";
+}
--- .github/workflows/pr-build-test-gradle-plugin-tests.ps1
+++ .github/workflows/pr-build-test-gradle-plugin-tests.ps1
@@ -1,10 +1,23 @@
-$projectNames = @(
-    "gobley-gradle",
-    "gobley-gradle-cargo",
-    "gobley-gradle-rust",
-    "gobley-gradle-uniffi"
-);
-foreach ($projectName in $projectNames) {
-    & "./gradlew" ":build-logic:${projectName}:test";
+$ErrorActionPreference = "Stop";
+
+$error = false;
+try {
+    $projectNames = @(
+        "gobley-gradle",
+        "gobley-gradle-cargo",
+        "gobley-gradle-rust",
+        "gobley-gradle-uniffi"
+    );
+    foreach ($projectName in $projectNames) {
+        & "./gradlew" ":build-logic:${projectName}:test";
+        if ($LASTEXITCODE -ne 0) {
+            $error = $true;
+        }
+    }
+} finally {
+    ./.github/workflows/pr-build-test-copy-test-result.ps1;
+    if ($error) {
+        exit 1;
+    }
+    exit 0;
 }
-./.github/workflows/pr-build-test-copy-test-result.ps1;
