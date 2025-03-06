c387e10dfba3b80d9581e7de1b59431f43bdea3c
Chanjung Kim <chanjungkim@paxbun.dev>
2025-03-06 21:30:04 +0900

    Test for WASM in Coverall

--- tests/uniffi/coverall/build.gradle.kts
+++ tests/uniffi/coverall/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     id("uniffi-tests-from-library")
 }
@@ -7,4 +9,14 @@ kotlin {
         nodejs()
         browser()
     }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        nodejs()
+        browser()
+    }
+    // Not supported by io.kotest:kotest-assertions-core:5.9.1
+    // @OptIn(ExperimentalWasmDsl::class)
+    // wasmWasi {
+    //     nodejs()
+    // }
 }
