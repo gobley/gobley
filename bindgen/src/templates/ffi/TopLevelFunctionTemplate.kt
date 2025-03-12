f5acd1063e4203953c46d1a57cfe0538e1357281
Chanjung Kim <chanjungkim@paxbun.dev>
2025-03-13 01:00:19 +0900

    Don't emit expect/actual for with a single Android or JVM target

--- bindgen/src/templates/ffi/TopLevelFunctionTemplate.kt
+++ bindgen/src/templates/ffi/TopLevelFunctionTemplate.kt
@@ -1,1 +1,5 @@
-{%- call kt::func_decl_with_body("actual", func, 0) %}
+{%- if config.single_kotlin_target() -%}
+{%- call kt::func_decl_with_body("", func, 0) -%}
+{%- else -%}
+{%- call kt::func_decl_with_body("actual", func, 0) -%}
+{%- endif %}
