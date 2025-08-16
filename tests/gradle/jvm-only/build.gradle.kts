155f8d7c230febe654a75fe12acbbe737dc62233
Chanjung Kim <chanjungkim@paxbun.dev>
2025-08-16 23:25:49 +0900

    Set Java compatibility in :tests:gradle:jvm-only

--- tests/gradle/jvm-only/build.gradle.kts
+++ tests/gradle/jvm-only/build.gradle.kts
@@ -14,6 +14,11 @@ cargo {
     }
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_17
