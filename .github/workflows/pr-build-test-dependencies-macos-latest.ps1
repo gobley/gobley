4ff4a322651d19da326ec489d23cccbec560004e
Chanjung Kim <chanjungkim@paxbun.dev>
2025-09-07 00:06:55 +0900

    Install additional dependencies

---
+++ .github/workflows/pr-build-test-dependencies-macos-latest.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = "Stop";
+$PSNativeCommandUseErrorActionPreference = $true;
+
+brew update;
+brew install mingw-w64;
---
+++ .github/workflows/pr-build-test-dependencies-ubuntu-latest.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = "Stop";
+$PSNativeCommandUseErrorActionPreference = $true;
+
+sudo apt-get update;
+sudo apt-get install -y mingw-w64;
