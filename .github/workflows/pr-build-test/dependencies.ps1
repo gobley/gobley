5e2bb91df4e4f01c323f4709fb09ef85423055c9
Chanjung Kim <chanjungkim@paxbun.dev>
2025-09-07 17:11:48 +0900

    Use MSYS2 Perl on Windows

--- .github/workflows/pr-build-test/dependencies.ps1
+++ .github/workflows/pr-build-test/dependencies.ps1
@@ -3,8 +3,12 @@ $PSNativeCommandUseErrorActionPreference = $true;
 
 if ($IsWindows) {
     choco install -y mingw;
-    # Required by :examples:tokio-blake3-app to build OpenSSL
-    choco install -y strawberryperl;
+    # Required by :examples:tokio-blake3-app to use Perl to build OpenSSL
+    choco install -y msys2;
+    # Check if OpenSSL will complain about backslashes with the perl shipped with MSYS2
+    & "C:\tools\msys64\usr\bin\perl.exe" -MFile::Spec::Functions=rel2abs,abs2rel -e "abs2rel(rel2abs('.')) =~ m{\\\\} and die 'This perl does not produce Unix-like paths';"
+    # Prepend the path to the directory containing MSYS2 Perl to PATH
+    "C:\tools\msys64\usr\bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append;
     # Required by :tests:gradle:android-linking
     choco install -y nasm;
 } elseif ($IsMacOS) {
