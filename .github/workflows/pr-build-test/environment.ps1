ad4d299847365ad2c52b2df5674f5d9c10663c3e
Chanjung Kim <chanjungkim@paxbun.dev>
2025-09-15 12:10:27 +0900

    Increase the metaspace size on Intel macOS

--- .github/workflows/pr-build-test/environment.ps1
+++ .github/workflows/pr-build-test/environment.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = "Stop";
 $PSNativeCommandUseErrorActionPreference = $true;
 
-# Disable Gradle Daemon on Intel macOS
+# Disable Gradle Daemon on Intel macOS and increase the metaspace size to 3GB
 if ($IsMacOS) {
     $currentArchitecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture;
     if ($currentArchitecture -eq [System.Runtime.InteropServices.Architecture]::X64) {
         ${env:ORG_GRADLE_PROJECT_org.gradle.daemon} = "false";
+        ${env:ORG_GRADLE_PROJECT_org.gradle.jvmargs} = "-Xmx6g -XX:MaxMetaspaceSize=3g -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8";
     }
 }
