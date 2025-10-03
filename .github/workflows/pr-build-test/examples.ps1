f329c4ce08ddab2a54eb0fce98b1cd52bb1f4ade
Chanjung Kim <chanjungkim@paxbun.dev>
2025-10-03 13:58:11 +0900

    Run example build tests in parallel

--- .github/workflows/pr-build-test/examples.ps1
+++ .github/workflows/pr-build-test/examples.ps1
@@ -1,34 +1,35 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$TestNames
+);
+
 $ErrorActionPreference = "Stop";
 $PSNativeCommandUseErrorActionPreference = $true;
 
 ./.github/workflows/pr-build-test/environment.ps1;
 
-# Run build project-wise to prevent no space left errors
-$projects = Get-ChildItem ./examples |
-    ? { Test-Path "$_/build.gradle.kts" } |
-    % { $_.Name };
-try {
-    foreach ($project in $projects) {
-        try {
-            ./gradlew ":examples:${project}:build" `
-                "-Pgobley.projects.gradleTests=false" `
-                "-Pgobley.projects.uniffiTests=false" `
-                "-Pgobley.projects.uniffiTests.extTypes=false" `
-                "-Pgobley.projects.uniffiTests.futures=false";
-        } finally {
-            ./.github/workflows/pr-build-test/copy-test-result.ps1;
-            ./gradlew --stop;
-            ./gradlew ":examples:${project}:clean" `
-                "-Pgobley.projects.gradleTests=false" `
-                "-Pgobley.projects.uniffiTests=false" `
-                "-Pgobley.projects.uniffiTests.extTypes=false" `
-                "-Pgobley.projects.uniffiTests.futures=false";
-        }
+$knownTests = @("app", "audioCppApp", "customTypes", "tokioBlake3App", "tokioBoringApp");
+foreach ($testName in $TestNames) {
+    if (-not ($knownTests -contains $testName)) {
+        throw "$testName is not a valid test name. Possible values: $knownTests";
     }
-    
+}
+$arguments = $knownTests | % {
+    $enable = ($TestNames -contains $_).ToString().ToLower();
+    "-Pgobley.projects.examples.${_}=$enable"
+};
+
+try {
+    ./gradlew build `
+        $arguments `
+        "-Pgobley.projects.gradleTests=false" `
+        "-Pgobley.projects.uniffiTests=false" `
+        "-Pgobley.projects.uniffiTests.extTypes=false" `
+        "-Pgobley.projects.uniffiTests.futures=false";
 } finally {
     ./gradlew --stop;
     ./gradlew clean `
+        $arguments `
         "-Pgobley.projects.gradleTests=false" `
         "-Pgobley.projects.uniffiTests=false" `
         "-Pgobley.projects.uniffiTests.extTypes=false" `
